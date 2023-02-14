babbd12fa1d77fc596586c6f55eeadcfb65413f4
Your Name <you@example.com>
2023-02-14 16:21:22 +0530

    Player is rolling the dice

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -7,6 +7,7 @@ namespace SnakeAndLadderGame
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
+            Snake.Ladder();
            
             Console.ReadLine();
         }
